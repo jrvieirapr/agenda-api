60bfb07dfd2199bd26a7ebbe2a3042b14b47e897
jrvieirapr <juliano.vieira@docente.pr.senac.br>
2023-08-03 13:49:19 -0300

    Ajuste Tarefa

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -19,14 +19,6 @@ class TarefaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTarefaRequest $request)
@@ -52,13 +44,6 @@ class TarefaController extends Controller
         return response()->json($tarefa);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tarefa $tarefa)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TarefaController;
 use App\Http\Controllers\TipoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
